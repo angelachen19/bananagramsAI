8819a8590b74edeb02c5468d9daec6a80965d5cd
Angela <ac2677@cornell.edu>
2023-05-17 14:24:04 -0400

    DAWG Implementation and Testing

--- algos.py
+++ algos.py
@@ -1,14 +1,95 @@
-POINTS = {'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 's': 1, 't': 1, 'r': 1,
-          'd': 2, 'g': 2,
-          'b': 3, 'c': 3, 'm': 3, 'p': 3,
-          'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4,
-          'k': 5,
-          'j': 8, 'x': 8,
-          'q': 10, 'z': 10
-          }
+import dawg
+
+# start of DAWG Implementation from scrabble-solver by aydinschwa on github https://github.com/aydinschwa/Scrabble-Solver/ for DAWG implementation
+
+# the class of the node in DAWG
+# TODO build this class out more
 
 
-def calculate_score(word):
+class Node:
+    next_id = 0
+
+    # what happens when class is initiated
+    def __init__(self):
+        self.terminal = False
+        self.children = {}
+        self.id = Node.next_id
+        Node.next_id = Node.next_id + 1
+
+
+def len_overlapping_pref(prev_word, curr_word):
+    # find length of overlapping prefix
+    len_overlapping_pref = 0
+    for l1, l2 in (zip(prev_word, curr_word)):
+        if l1 == l2:
+            len_overlapping_pref = len_overlapping_pref + 1
+        else:
+            return len_overlapping_pref
+    return len_overlapping_pref
+
+
+# minimize: iterating from the final node of current word to where the curr word and prev word have the same prefix
+def min(curr_node, len_overlapping_pref, checked_nodes, unchecked_nodes):
+    len_unchecked_nodes = len(unchecked_nodes)
+    for x in range(len_unchecked_nodes-1, len_overlapping_pref-1, -1):
+        parent, letter, child = unchecked_nodes[x]
+        if child in checked_nodes:
+            parent.children[letter] = unchecked_nodes[child]
+        else:
+            checked_nodes[child] = child
+        unchecked_nodes.pop()
+        curr_node = parent
+    return curr_node
+
+
+def create_dawg(dictionary):
+    root = Node()
+    curr_node = root
+    prev_word = ""
+    checked_nodes = {root: root}
+    unchecked_nodes = []
+
+    for word_index, curr_word in enumerate(dictionary):
+        len_pref = len_overlapping_pref(prev_word, curr_word)
+
+        # if there are unchecked nodes, minimize ie go back to the pref that is on the dawg alr
+        if unchecked_nodes:
+            curr_node = min(curr_node, len_pref,
+                            checked_nodes, unchecked_nodes)
+
+        # adding the new nodes after nodes already on the dawg
+        for l in curr_word[len_pref:]:
+            # create a new node
+            next_node = Node()
+            curr_node.children[l] = next_node
+            unchecked_nodes.append((curr_node, l, next_node))
+            curr_node = next_node
+
+        # finished checking the entire word
+        curr_node.terminal = True
+        prev_word = curr_word
+
+    min(curr_node, 0, checked_nodes, unchecked_nodes)
+    # Test
+    print(len(checked_nodes))
+    return root
+
+
+def valid_word(word, curr_node):
+    for l in word:
+        if l not in curr_node.children:
+            return False
+        curr_node = curr_node.children[l]
+
+    if curr_node.terminal == True:
+        return True
+    else:
+        return False
+
+# end of DAWG Implementation from scrabble-solver by aydinschwa on github https://github.com/aydinschwa/Scrabble-Solver/ for DAWG implementation
+
+
+def score(word):
     """
     Finds the score of a word based on the scrabble system
 
@@ -22,97 +103,58 @@ def calculate_score(word):
     """
     word = word.lower()
     score = 0
+    points_dict = {'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 's': 1, 't': 1, 'r': 1,
+                   'd': 2, 'g': 2,
+                   'b': 3, 'c': 3, 'm': 3, 'p': 3,
+                   'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4,
+                   'k': 5,
+                   'j': 8, 'x': 8,
+                   'q': 10, 'z': 10
+                   }
     for i in word:
-        score += POINTS[i]
+        score += points_dict[i]
     return score
 
 
-class Node:
+def next_best_word(tiles, seed, curr_board, dawg):
     """
-    Represents a node in DAWG. Each node has a list of its edges to other nodes
-    Nodes are equivalent if they have the same edges leading to the same states. 
-    It can be used as a key in a dictionary with the __hash__ and__eq__ 
-    functions.
-    """
-    next_id = 0
-
-    def __init__(self):
-        # terminal = a word ends at that location
-        self.is_terminal = False
-        self.id = Node.next_id
-        Node.next_id += 1
-        self.edges = {}
-
-    def __str__(self):
-        out = [f"Node {self.id}\nChildren:\n"]
-        letter_child_dict = self.children.items()
-        for letter, child in letter_child_dict:
-            out.append(f" {letter} -> {child.id}\n")
-        return " ".join(out)
-
-    def __repr__(self):
-        out = []
-        if self.is_terminal:
-            out.append("1")
-        else:
-            out.append("0")
-        for key, val in self.edges.items():
-            out.append(key)
-            out.append(str(val.id))
-        return "_".join(out)
-
-    def __hash__(self):
-        return self.__repr__().__hash__()
-
-    def __eq__(self, other):
-        return self.__repr__() == other.__repr__()
-
-
-def create_dawg(dict):
-    """
-    Create a directed acyclic word graph (DAWG) and output to a textfile
-    """
-    # create root (parent for tree)
-    root = Node()
-
-
-def search_dawg(word, node):
-    """
-    Recursively find a word in DAWG, starting from a certain node
-    """
-    pass
-
-
-def best_first_word(search_dawg=search_dawg(), calculate_score=calculate_score):
-    """
-    Finding the best first word given the set of tiles. Best meaning the maximum 
-    score based on the scrabble scoring. Here we call on dawg with the seed nodes
-    as nothing.
+    Finding the set of possible next words given the set of tiles in the order 
+    highest to lowest score based on the scrabble scoring.
 
     Parameters
     =======
-
-
-    Returns
-    =======
-    new_board: 2D matrix, state of the board with the best first word
-    """
-    # the board is empty and you can create any world from tile hand
-    pass
-
-
-def heuristic(curr_board, tile_pool, best_first_word=best_first_word):
-    """
-    Completing the curr_board with values from tile_pool with heuristics of 
-    best_first_word
-
-    Parameters
-    =======
-    curr_board: 2D matrix, current state of board
-    tile_pool: tiles to draw from
+    tiles: (list) list of letters in hand that can be played
+    curr_board: ([[array]]) 2D matrix, will act as the seed
+    dawg: dawg
 
     Returns
     =======
-    new_board: 2D matrix, state of the board with the best first word
+    words: (list) list of strings, where score(word[i]) >= score(word[i+1])
     """
+    # find the longest word given the seed and set of tiles
+    # check if word is in DAWG
+    # best_word = ''
+    # highest_score = 0
+
+    # for i in range(len(tiles)):
+    #     for j in range(i+1, len(tiles)+1):
     pass
+
+# testing
+
+
+# def heuristic(curr_board, tile_pool, best_first_word=best_first_word):
+#     """
+#     Completing the curr_board with values from tile_pool with heuristics of
+#     best_first_word
+
+#     Parameters
+#     =======
+#     curr_board: 2D matrix, current state of board
+#     tile_pool: tiles to draw from
+
+#     Returns
+#     =======
+#     new_board: 2D matrix, state of the board with the best first word
+#     """
+#     pass
--- tests.py
+++ tests.py
@@ -1,2 +1,36 @@
+from algos import *
+import unittest
+
 # Check if we get the right number of tiles in hand
 # Check if the hand + new tile set = original 144 tile set
+
+
+class TestAlgos(unittest.TestCase):
+    # testing DAWG implementation
+    file = open("dictionary.txt", "r")
+    data = file.read()
+    scrabble_dict = data.split("\n")  # list of valid scrabble words
+    test_dawg_root = create_dawg(scrabble_dict)
+
+    def test_dawg_1(self):
+        self.assertEqual(valid_word("ZEPPELIN", self.__class__.test_dawg_root),
+                         True, "ZEPPELIN is not in DAWG")
+
+    def test_dawg_2(self):
+        print(valid_word("MARROWY", self.__class__.test_dawg_root))
+        self.assertEqual(valid_word("MARROWY", self.__class__.test_dawg_root),
+                         True, "MARROWY is not in DAWG")
+
+    def test_dawg_3(self):
+        self.assertEqual(valid_word("BRANTAIL", self.__class__.test_dawg_root),
+                         True, "BRANTAIL is not in DAWG")
+
+    def test_dawg_4(self):
+        self.assertEqual(valid_word("ACEQUIAS", self.__class__.test_dawg_root),
+                         True, "ACEQUIAS is not in DAWG")
+
+    file.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
